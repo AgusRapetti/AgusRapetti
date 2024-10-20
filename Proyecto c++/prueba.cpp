7e4e2c917006002c4369953732abaf7e61ccd350
AgusRapetti <164506658+AgusRapetti@users.noreply.github.com>
2024-10-20 01:47:55 -0300

    Update prueba.cpp

--- Proyecto c++/prueba.cpp
+++ Proyecto c++/prueba.cpp
@@ -5,5 +5,11 @@ using namespace std;
 int main(){
 
 cout << "Hola, Mundo"
+cout << "Hola, Mundo"
+cout << "Hola, Mundo"
+cout << "Hola, Mundo"
+cout << "Hola, Mundo"
+cout << "Hola, Mundo"
+cout << "Hola, Mundo"
 return 0;
 }
